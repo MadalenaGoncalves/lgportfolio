8767ed4c47bead5a9750f2df2a08401b54dbac1b
madpg <cheetara63@gmail.com>
2016-04-01 22:23:12 +0100

    Corrected route paths

--- router.js
+++ router.js
@@ -3,15 +3,16 @@ var express = require('express');
 var router = express.Router();
 
 var path = require('path');
-var C_VIEW_PATH = path.join(__dirname, "public/static");
+var C_VIEW_PATH = path.join(__dirname, "/public/static");
 
 router.get('/', function (req, res) {
-    res.send('Hi! My name is Madalena');
+    res.sendFile(C_VIEW_PATH + "/index.html");
 });
  
-router.get('/home', function (req, res) {
-  res.sendFile(C_VIEW_PATH + "/index.html");
-});
+// router.get('/home', function (req, res) {
+//   res.sendFile(C_VIEW_PATH + "/index.html");
+// });
+
 router.get('/projects', function (req, res) {
   db.find(function (err,docs) {
       if(err) console.log("ERROR in project.find : " + err);
--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-// Database setup
-// var db = require('./db/db');
-// db.connect('mongodb://localhost/lgportfoliodb');
+// This file should be called app.js 
 
-// Server setup 
-var express = require('express');
-var app     = express();
-var http    = require('http');
+// Initialization of the express framework
+var express = require('express'),
+    path    = require('path'),
+    // bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    databaseName = 'lgportfolio';
+// var engine  = require('ejs-locals');
+// var querystring = require('querystring'); 
 
-var path    = require('path');
-var engine  = require('ejs-locals');
-var querystring = require('querystring'); 
-
-// all environments
+// Server setup
+var app = express();
 app.set( 'port', process.env.PORT || 3000 );
-// app.engine( 'ejs', engine );
-// app.set( 'views', path.join( __dirname, 'public' ));
-// app.set( 'view engine', 'ejs' );
+// app.use(bodyParser.json());                          // parse application/json
+// app.use(bodyParser.urlencoded({extended:true}));     // parse application/x-www-form-urlencoded
+    // app.engine( 'ejs', engine );
+    // app.set( 'views', path.join( __dirname, 'public' ));
+    // app.set( 'view engine', 'ejs' );
 
-
-// Middleware
+// Routers
 var routes = require('./router');
-app.use(express.static(path.join(__dirname, 'public/static')));
+app.use(express.static(__dirname)); // static path to root
+app.use(express.static(path.join(__dirname, 'public/static'))); // static path to index.html
+// app.use(express.static(path.join(__dirname, 'js'))); // static path to js folder in root
 app.use('/',routes);
 
+// Other middleware
 // app.use( express.favicon());
 // app.use( express.logger( 'dev' ));
 // app.use( express.cookieParser()); // use express cookies to recognize users
@@ -32,7 +35,15 @@ app.use('/',routes);
 // app.use( express.methodOverride());
 // app.use( express.errorHandler());
 
-// START SERVER
-http.createServer(app).listen(app.get('port'), function(){
-  console.log( 'Express server listening on port ' + app.get('port'));
-} );
+// Database setup
+mongoose.connect('mongodb://localhost/' + databaseName);
+var db = mongoose.connection;
+db.on(err,console.error);
+db.once('open',startServer);
+
+// Start up the server
+function startServer(){   
+    var server = app.listen(app.get('port'), function(){
+        console.log( 'Express server listening on port ' + app.get('port'));
+    });
+}
