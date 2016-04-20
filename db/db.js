5beb0a614aae027f1ee4e4ebbc9c1f9dcb9f33f9
madpg <cheetara63@gmail.com>
2016-04-20 22:20:17 +0100

    reorganized files and directories

--- db/db.js
+++ db/db.js
@@ -1,27 +1,27 @@
-var mongoose = require('mongoose');
-var mydb = mongoose.connect('mongodb://localhost/lgportfoliodb');
+// var mongoose = require('mongoose');
+// var mydb = mongoose.connect('mongodb://localhost/lgportfoliodb');
 
-var Schema   = mongoose.Schema;
+// var Schema   = mongoose.Schema;
 
-var ProjectSchema = new Schema({
-    name        : String,
-    description : String
-});
-var Project = mongoose.model('Project', ProjectSchema);
-
-var ImageSchema = new Schema({
-    id   : String,
-    path : String
-});
-mongoose.model('Image', ImageSchema);
-
-// GETTERS AND SETTERS
-var proj1 = new Project({name:'name_proj1',description:'desc_proj1'});
-// proj1.save( function (err){
-//     if(err) console.log("error on save proj1");
-//     console.log("proj1 saved");
+// var ProjectSchema = new Schema({
+//     name        : String,
+//     description : String
 // });
+// var Project = mongoose.model('Project', ProjectSchema);
+
+// var ImageSchema = new Schema({
+//     id   : String,
+//     path : String
+// });
+// mongoose.model('Image', ImageSchema);
+
+// // GETTERS AND SETTERS
+// var proj1 = new Project({name:'name_proj1',description:'desc_proj1'});
+// // proj1.save( function (err){
+// //     if(err) console.log("error on save proj1");
+// //     console.log("proj1 saved");
+// // });
 
 
-// EXPORTS
-module.exports = mydb.model('Project');
+// // EXPORTS
+// module.exports = mydb.model('Project');
